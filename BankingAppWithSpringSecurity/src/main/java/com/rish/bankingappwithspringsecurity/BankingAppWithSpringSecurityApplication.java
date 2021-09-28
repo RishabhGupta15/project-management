f36d3cbef60eadbc006eb7cfb5c6ea607e1edaeb
RishabhGupta15 <48063952+RishabhGupta15@users.noreply.github.com>
2021-09-28 13:15:11 +0530

    just commiting

---
+++ BankingAppWithSpringSecurity/src/main/java/com/rish/bankingappwithspringsecurity/BankingAppWithSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.rish.bankingappwithspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingAppWithSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingAppWithSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ BankingAppWithSpringSecurity/src/test/java/com/rish/bankingappwithspringsecurity/BankingAppWithSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rish.bankingappwithspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppWithSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
